d994761e7f52d73aba397938882fb829aca735cf
thiagompc <thiago099carvalho@gmail.com>
2021-06-14 12:54:43 -0300

    Created Footer and FooterTitle components

---
+++ ediaristas/src/ui/components/surfaces/Footer/Footer.style.tsx
@@ -0,0 +1,15 @@
+import { experimentalStyled as styled } from "@material-ui/core";
+import { Container, Typography } from "@material-ui/core";
+
+export const FooterStyled = styled("footer")`
+  background-color: ${({ theme }) => theme.palette.primary.main};
+  color: ${({ theme }) =>
+    theme.palette.getContrastText(theme.palette.primary.main)};
+  padding: ${({ theme }) => theme.spacing(4)} 0;
+`;
+
+export const FooterContainer = styled(Container)``;
+
+export const FooterTitle = styled((props) => (
+  <Typography component={"h2"} variant={"body2"} {...props} />
+))``;
---
+++ ediaristas/src/ui/components/surfaces/Footer/Footer.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { FooterStyled, FooterContainer, FooterTitle } from "./Footer.style";
+
+const Footer = () => {
+  return (
+    <FooterStyled>
+      <FooterContainer>
+        <FooterTitle>Quem somos</FooterTitle>
+      </FooterContainer>
+    </FooterStyled>
+  );
+};
+
+export default Footer;
