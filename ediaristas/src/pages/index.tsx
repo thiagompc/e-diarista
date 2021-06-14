7b223e7e50acdc5f87e608edeb549ecddf7bdf82
thiagompc <thiago099carvalho@gmail.com>
2021-06-14 17:57:53 -0300

    Finished textfield component and style

--- ediaristas/src/pages/index.tsx
+++ ediaristas/src/pages/index.tsx
@@ -1,6 +1,9 @@
 import SafeEnvironment from "ui/components/feedback/SafeEnvironment/SafeEnvironment";
 import PageTitle from "ui/components/data-display/PageTitle/PageTitle";
 import UserInformation from "ui/components/data-display/UserInformation/UserInformation";
+import TextFieldMask from "ui/components/inputs/TextFieldMask/TextFieldMask";
+import { Button, Typography } from "@material-ui/core";
+import { FormElementsContainer } from "@styles/pages/index.style";
 
 export default function Home() {
   return (
@@ -12,6 +15,22 @@ export default function Home() {
           "Preencha seu endereço e veja todos os profissionais da sua localidade"
         }
       />
+      <FormElementsContainer>
+        <TextFieldMask
+          mask={"99.999-999"}
+          label={"Digite seu CEP"}
+          fullWidth
+          variant={"outlined"}
+        />
+        <Typography color={"error"}>CEP inválido</Typography>
+        <Button
+          variant={"contained"}
+          color={"secondary"}
+          sx={{ width: "220px" }}
+        >
+          Buscar
+        </Button>
+      </FormElementsContainer>
 
       <UserInformation
         name={"Thiago Mesquita"}
@@ -20,9 +39,9 @@ export default function Home() {
         description={"Brasília"}
       />
       <UserInformation
-        name={"Thiago Mesquita"}
-        picture={"https://github.com/thiagompc"}
-        rating={3.5}
+        name={"Beatriz Liarte"}
+        picture={"B"}
+        rating={5}
         description={"Brasília"}
       />
     </div>
