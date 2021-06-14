7e167686673e9e908ff8eb92b1830b2995918032
thiagompc <thiago099carvalho@gmail.com>
2021-06-14 19:38:57 -0300

    Created ProfissionaisPaper and ProfissionaisContainer components

--- ediaristas/src/pages/index.tsx
+++ ediaristas/src/pages/index.tsx
@@ -2,8 +2,12 @@ import SafeEnvironment from "ui/components/feedback/SafeEnvironment/SafeEnvironm
 import PageTitle from "ui/components/data-display/PageTitle/PageTitle";
 import UserInformation from "ui/components/data-display/UserInformation/UserInformation";
 import TextFieldMask from "ui/components/inputs/TextFieldMask/TextFieldMask";
-import { Button, Typography } from "@material-ui/core";
-import { FormElementsContainer } from "@styles/pages/index.style";
+import { Button, Typography, Container } from "@material-ui/core";
+import {
+  FormElementsContainer,
+  ProfissionaisPaper,
+  ProfissionaisContainer,
+} from "@styles/pages/index.style";
 
 export default function Home() {
   return (
@@ -15,35 +19,64 @@ export default function Home() {
           "Preencha seu endereço e veja todos os profissionais da sua localidade"
         }
       />
-      <FormElementsContainer>
-        <TextFieldMask
-          mask={"99.999-999"}
-          label={"Digite seu CEP"}
-          fullWidth
-          variant={"outlined"}
-        />
-        <Typography color={"error"}>CEP inválido</Typography>
-        <Button
-          variant={"contained"}
-          color={"secondary"}
-          sx={{ width: "220px" }}
-        >
-          Buscar
-        </Button>
-      </FormElementsContainer>
-
-      <UserInformation
-        name={"Thiago Mesquita"}
-        picture={"https://github.com/thiagompc.png"}
-        rating={3.5}
-        description={"Brasília"}
-      />
-      <UserInformation
-        name={"Beatriz Liarte"}
-        picture={"B"}
-        rating={5}
-        description={"Brasília"}
-      />
+      <Container>
+        <FormElementsContainer>
+          <TextFieldMask
+            mask={"99.999-999"}
+            label={"Digite seu CEP"}
+            fullWidth
+            variant={"outlined"}
+          />
+          <Typography color={"error"}>CEP inválido</Typography>
+          <Button
+            variant={"contained"}
+            color={"secondary"}
+            sx={{ width: "220px" }}
+          >
+            Buscar
+          </Button>
+        </FormElementsContainer>
+        <ProfissionaisPaper>
+          <ProfissionaisContainer>
+            <UserInformation
+              name={"Thiago Mesquita"}
+              picture={"https://github.com/thiagompc.png"}
+              rating={3.5}
+              description={"Brasília"}
+            />
+            <UserInformation
+              name={"Beatriz Liarte"}
+              picture={"B"}
+              rating={5}
+              description={"Brasília"}
+            />
+            <UserInformation
+              name={"Thiago Mesquita"}
+              picture={"https://github.com/thiagompc.png"}
+              rating={3.5}
+              description={"Brasília"}
+            />
+            <UserInformation
+              name={"Beatriz Liarte"}
+              picture={"B"}
+              rating={5}
+              description={"Brasília"}
+            />
+            <UserInformation
+              name={"Thiago Mesquita"}
+              picture={"https://github.com/thiagompc.png"}
+              rating={3.5}
+              description={"Brasília"}
+            />
+            <UserInformation
+              name={"Beatriz Liarte"}
+              picture={"B"}
+              rating={5}
+              description={"Brasília"}
+            />
+          </ProfissionaisContainer>
+        </ProfissionaisPaper>
+      </Container>
     </div>
   );
 }
